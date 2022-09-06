e07228e7a5e08da9bf0baaffc4aee14781cebaa5
nico.ismaili <nico.ismaili@aoe.com>
2022-09-06 16:36:38 +0200

    [Add] Test with mock data

---
+++ mockData_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/nico-i/profile-card-gen/types"
+	"testing"
+)
+
+func TestGeneratePDF(t *testing.T) {
+	data := types.TemplateData{
+		User: types.User{
+			Firstname:   "Max",
+			Lastname:    "Mustermann",
+			Role:        "Chef",
+			City:        "Berlin, DE",
+			Team:        "The best team",
+			WorksAt:     "Hamburg HQ",
+			Hometown:    "Buxdehude",
+			Quote:       "Good quote.exe",
+			Photo:       "https://picsum.photos/500/500",
+			HasWorkedAt: []string{"for suxess", "AStA of the RheinMain\nUniversity of Applied Science"},
+			Skills:      []string{"Web development", "Automation", "Graphic design"},
+			Interests:   []string{"Photography", "Guitar", "Machine Learning"},
+			Other:       []string{"Always open to conversation", "Excited to learn"},
+		},
+		BasePath: "https://www.example.com",
+	}
+	_, err := GeneratePDF(&data, "./public/templates/aoe-profile-card.html")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
